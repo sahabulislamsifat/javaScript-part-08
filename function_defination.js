166f7916e3f4bfea6058e3add38ce5247f73d686
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-26 03:45:49 +0600

    javaScript function definations

---
+++ function_defination.js
@@ -0,0 +1,19 @@
+// // (function () {
+// //   console.log("I am function...");
+// // })(); // self invoking function......
+
+// function myFunction(a, b) {
+//   console.log("hello");
+//   //   return a * b;
+//   // return undefined
+// }
+
+// const result = myFunction(3, 4);
+// console.log(result);
+
+const x = function (a, b) {
+  console.log(arguments);
+  return a * b;
+};
+
+console.log(x(3, 4));
